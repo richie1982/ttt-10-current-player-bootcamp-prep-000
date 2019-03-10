b9ade5e9a39be6810c1a9fd0cacb1205c8332cc8
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-10 14:36:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,7 @@
 def turn_count(board)
   counter = 0 
-  board.each? == "X" || board.each? == "O" do counter += 1
-end
+  board.each do |icon|
+  if icon == "X" || icon == "O" 
+    counter += 1
+  end
 end
